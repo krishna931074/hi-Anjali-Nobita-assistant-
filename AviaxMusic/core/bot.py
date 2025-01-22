5434c7a22f56f86eb6c945877611255ef545d6d9
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-23 05:23:26 +0530

    Update bot.py

--- AviaxMusic/core/bot.py
+++ AviaxMusic/core/bot.py
@@ -17,9 +17,8 @@ class Aviax(Client):
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
-            in_memory=True,
-            parse_mode=ParseMode.HTML,
-            max_concurrent_transmissions=7,
+            in_memory=True,
+         max_concurrent_transmissions=7,
         )
 
     async def start(self):
