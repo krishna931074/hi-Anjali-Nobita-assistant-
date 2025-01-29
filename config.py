debfc10482cbb5dd0c0fb712deb666c8a56ed843
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-29 22:47:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -39,7 +39,7 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/nobi_bots")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/noxarion_network")
 
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/anime_societyy")
 
